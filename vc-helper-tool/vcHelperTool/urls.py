716424d770e4bd9bb7ef8955162578075eb2d2c6
masuda681 <atsushi.masuda.cb@east.ntt.co.jp>
2025-07-03 10:05:08 +0900

    Add files via upload
    first commit

---
+++ vc-helper-tool/vcHelperTool/urls.py
@@ -0,0 +1,8 @@
+﻿from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.hello_world),
+]
---
+++ vc-helper-tool/vcHelperTool/views.py
@@ -0,0 +1,4 @@
+﻿from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse("Hello, World!")
